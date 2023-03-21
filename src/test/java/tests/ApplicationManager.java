b13ed5bb79256450aef526881902401c81ec3f7e
Alexandra Balitskaya <a.balickaya@gmail.com>
2023-03-21 20:52:49 +0400

    Changed in build.gradle 'webdrivermanager', version: '3.2.0'

--- src/test/java/tests/ApplicationManager.java
+++ src/test/java/tests/ApplicationManager.java
@@ -6,49 +6,43 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
-import java.net.URI;
-
+import java.net.URL;
 
 public class ApplicationManager {
 
     public WebDriver driver;
 
-
     public WebDriver remoteDriverSelenoid() throws MalformedURLException {
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setBrowserName("firefox");
-        capabilities.setVersion("90.0");
-        capabilities.setCapability("enableVNC", true);
-        capabilities.setCapability("enableLog", true);
-        driver = new RemoteWebDriver(
-                URI.create("http://127.0.0.1:4444/wd/hub").toURL(),
-                capabilities);
-        return driver;
+        FirefoxOptions options = new FirefoxOptions();
+        options.setCapability("browserName", "firefox");
+        options.setCapability("browserVersion", "90.0");
+        options.setCapability("enableVNC", true);
+        options.setCapability("enableLog", true);
+
+        return new RemoteWebDriver(new URL("http://127.0.0.1:4444/wd/hub"), options);
     }
 
     protected void init(boolean useRemoteDriver) throws MalformedURLException {
         if (useRemoteDriver == true) {
             driver = remoteDriverSelenoid();
-            System.out.println("Using remote driver(Selenoid)");
+            System.out.println("Using remote driver (Selenoid)");
         } else {
             WebDriverManager.firefoxdriver().setup();
-            driver = new FirefoxDriver();
+            //
+            driver = new FirefoxDriver(); //
             System.out.println("Using local FirefoxDriver");
         }
 
-
-        //driver = remoteDriverSelenoid();
-
         driver.get("https://www.globalsqa.com/angularJs-protractor/BankingProject/");
         driver.manage().window().maximize();
-
     }
+
 
     public String takeScreenshot() throws IOException {
         File tmp = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,34 +1,29 @@
 package tests;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 
-import java.io.IOException;
 import java.lang.reflect.Method;
 import java.net.MalformedURLException;
+import java.util.Arrays;
 
 public class TestBase {
 
     static Logger logger = LoggerFactory.getLogger(TestBase.class);
     protected static ApplicationManager app = new ApplicationManager();
 
-
-    @BeforeClass
-    public static void setUp() {
-        WebDriverManager.firefoxdriver().setup(); //Драйверы для хрома (можно для любого браузера
-        logger.info("Setup firefox driver");
-    }
-
     @BeforeMethod
-    public void setupTest(Method m, Object[] p) throws MalformedURLException {
+    public void setupTest() throws MalformedURLException {
         app.init(false);
     }
 
+    @BeforeMethod
+    public void startTest(Method m, Object[] p) {
+        logger.info("Start test " + m.getName() + " with data: " + Arrays.asList(p));
+    }
 
     @AfterMethod
     public void tearDown() {
@@ -36,20 +31,13 @@ public class TestBase {
     }
 
     @AfterMethod
-    public void stopTest(ITestResult result) throws IOException {
+    public void stopTest(ITestResult result) {
         if (result.isSuccess()) {
-            logger.info("PASSED" + result.getMethod().getMethodName()); // + app.getRegister().deleteFiles("records"));
+            logger.info("PASSED" + result.getMethod().getMethodName());
         } else {
-            logger.info("FAILED" + result.getMethod().getMethodName()); // + "Screenshot path: " + app.takeScreenshot());
+            logger.info("FAILED" + result.getMethod().getMethodName());
         }
 
         logger.info("=========================================================================");
     }
-
-
-    /*protected void stop() {
-        driver.quit();
-    }*/
-
-
 }
